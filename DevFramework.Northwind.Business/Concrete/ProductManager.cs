f42aebf7a6128ec1190bb54c1006a54e920a9308
muhammet demiralay <pepmuhammet.42@gmail.com>
2023-07-20 22:28:45 +0300

    Aspect and Cross Cutting Concerns added for fluent validation. Added ninject for dependencies.

---
+++ DevFramework.Northwind.Business/Concrete/ProductManager.cs
@@ -0,0 +1,36 @@
+﻿using DevFramework.Northwind.Business.Abstract;
+using DevFramework.Northwind.Dataccess.Abstract;
+using DevFramework.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void Add(Product product)
+        {
+            _productDal.Add(product);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetList();
+        }
+
+        public Product GetById(int id)
+        {
+            return _productDal.Get(p => p.ProductId == id);
+        }
+    }
+}
---
+++ DevFramework.Northwind.Business/DependencyResolvers/Ninject/NinjectBusinessModule.cs
@@ -0,0 +1,26 @@
+﻿using DevFramework.Core.DataAccess.NHibernate;
+using DevFramework.Northwind.Business.Abstract;
+using DevFramework.Northwind.Business.Concrete;
+using DevFramework.Northwind.Dataccess.Abstract;
+using DevFramework.Northwind.Dataccess.Concrete.EntityFramework;
+using DevFramework.Northwind.Dataccess.Concrete.NHibernate.Helper;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.Business.DependencyResolvers.Ninject
+{
+    public class NinjectBusinessModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IProductService>().To<ProductManager>().InSingletonScope();
+            Bind<IProductDal>().To<EFProductDal>();
+
+            Bind<NHibernateHelper>().To<SqlServerHelper>();
+        }
+    }
+}
